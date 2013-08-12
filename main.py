4597f97e15959f0ea7a6b1dcf9671dc28c7d81fe
Jack Dunn <jack.dunn.nz@gmail.com>
2013-08-12 14:35:41 +1200

    Documentation for file read method

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 # Read in input file
+def ReadInputFile(filename):
+"""Reads input file containing problem data.
+
+First line of the input file contains the number of variables. All of these
+are assumed to be binary (0-1) variables.
+
+The second line contains the coefficients of each variable in the single
+knapsack constraint, delimited by commas.
+
+The third line contains the right hand side value for the knapsack constraint.
+This value is assumed to be integer.
+TODO(jwd): check if this requirement is necessary.
+
+Inputs:
+    filename: A string, the filename of the input file to read.
+
+Returns:
+    A list, the coefficients for the knapsack constraint.
+    A number, the value for the right hand side in the knapsack constraint.
+
+"""
+    pass
 
 # Order set of coefficients
 
