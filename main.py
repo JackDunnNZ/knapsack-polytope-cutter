89641139505142618742f9fcf8e6a71347e46c79
Jack Dunn <jack.dunn.nz@gmail.com>
2013-08-12 23:40:52 +1200

    Efficient backtrack search for minimal sets. Filters out non-strong minimal covers also/

--- main.py
+++ main.py
@@ -51,25 +51,59 @@ def ConstructOrderedSet(A, b):
 def SumCoeffsOverSet(summing_set, A):
     return sum(A[i - 1] for i in summing_set)
 
+def SumCoeffsOverIndexList(summing_list, A):
+    return sum(A[i] * summing_list[i] for i in range(len(summing_list)))
+
+def ConvertIndexListToSet(index_list):
+    return set(i + 1 for i, j in enumerate(index_list) if j == 1)
+
+def ExtendSet(index_set):
+    min_elem = min(index_set)
+    return index_set.union(set(range(1, min_elem)))
+
 # Find min covers of constraint
 def GenerateMinimalCovers(N, A, b):
     sets = []
     set_map = defaultdict(list)
-    for subset in powerset(N):
-        if SumCoeffsOverSet(subset, A) > b:
-            print subset, SumCoeffsOverSet(subset, A)
-            sets.append(subset)
+
+    n = len(N)
+    s = [0 for _ in range(n)]
+
+    k = 0
+    while k < n:
+        print s, k
+        v = SumCoeffsOverIndexList(s, A)
+        s[k] += 1
+        if v + A[k] > b:
+            subset = ConvertIndexListToSet(s)
             set_map[len(subset)].append(subset)
+            s[k] = 0
+        k += 1
+        if k == n:
+            if v == 0:
+                break
+            k -= 1
+            s[k] = 0
+            while s[k] != 1:
+                s[k] = 0
+                k -= 1
+            s[k] = 0
+            k += 1
 
     # Filter out all non strong covers
-    for set_length, subsets in set_map.iteritems():
-        l = len(subsets)
-        for i, subset in enumerate(reversed(subsets)):
-            for j in range(i, l):
-                print l, i, j
-                if subset < subsets[l - 1 - j]:
-                    subsets.remove(subset)
-        print subsets
+    for subsets in set_map.itervalues():
+        extended_subsets = [ExtendSet(ss) for ss in subsets]
+        l = len(subsets) - 1
+        for i in range(len(subsets)):
+            for j in range(len(subsets)):
+                if extended_subsets[l - i] < extended_subsets[l - j]:
+                    subsets.pop(l-i)
+                    break
+
+        sets += subsets
+
+    return sets
+
 
 # Generate constraint from each strong cover
 
@@ -80,4 +114,4 @@ def GenerateMinimalCovers(N, A, b):
 # Main routine (to be moved)
 A, b = ReadInputFile('example_problem.dat')
 N = ConstructOrderedSet(A, b)
-GenerateMinimalCovers(N, A, b)
+sets = GenerateMinimalCovers(N, A, b)
