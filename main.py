125a1a6ff9b81ce28046bf311a052a42dec9f57c
Jack Dunn <jack.dunn.nz@gmail.com>
2013-08-12 21:04:41 +1200

    File read complete

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
+
 # Read in input file
 def ReadInputFile(filename):
-"""Reads input file containing problem data.
+    """Reads input file containing problem data.
 
-First line of the input file contains the number of variables. All of these
-are assumed to be binary (0-1) variables.
+    First line of the input file contains the number of variables. All of these
+    are assumed to be binary (0-1) variables.
 
-The second line contains the coefficients of each variable in the single
-knapsack constraint, delimited by commas.
+    The second line contains the coefficients of each variable in the single
+    knapsack constraint, delimited by commas.
 
-The third line contains the right hand side value for the knapsack constraint.
-This value is assumed to be integer.
-TODO(jwd): check if this requirement is necessary.
+    The third line contains the right hand side value for the knapsack constraint.
+    This value is assumed to be integer.
+    TODO(jwd): check if this requirement is necessary.
 
-Inputs:
-    filename: A string, the filename of the input file to read.
+    Inputs:
+        filename: A string, the filename of the input file to read.
 
-Returns:
-    A list, the coefficients for the knapsack constraint.
-    A number, the value for the right hand side in the knapsack constraint.
+    Returns:
+        A list, the coefficients for the knapsack constraint.
+        A number, the value for the right hand side in the knapsack constraint.
+    """
 
-"""
-    pass
+    f = open(filename, 'r')
+
+    # Read number of coefficients
+    n = int(f.readline().rstrip('\n'))
+
+    # Read in n coefficients
+    A = [int(a) for a in f.readline().rstrip('\n').split(',')[0:n]]
+
+    # Read RHS
+    b = int(f.readline().rstrip('\n'))
+
+    f.close()
+    return A, b
 
 # Order set of coefficients
 
@@ -33,3 +46,6 @@ Returns:
 # Check constraints for facet defining condition
 
 # Generate output file
+
+# Main routine (to be moved)
+A, b = ReadInputFile('example_problem.dat')
