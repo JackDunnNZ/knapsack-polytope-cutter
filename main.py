8dd7c6a618e66ecc2052b60979e27aa66a23075e
Jack Dunn <jack.dunn.nz@gmail.com>
2013-08-12 14:27:34 +1200

    Outline

---
+++ main.py
@@ -0,0 +1,13 @@
+# Read in input file
+
+# Order set of coefficients
+
+# Find min covers of constraint
+
+# Filter out all non strong covers
+
+# Generate constraint from each strong cover
+
+# Check constraints for facet defining condition
+
+# Generate output file
